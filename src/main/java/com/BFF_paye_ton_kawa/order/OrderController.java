49c4538af0016c3d616049673107a0559d12b743
Eliseo <eliseomurillo9@gmail.com>
2025-06-11 12:09:10 +0200

    refactor: error handler

--- src/main/java/com/BFF_paye_ton_kawa/order/OrderController.java
+++ src/main/java/com/BFF_paye_ton_kawa/order/OrderController.java
@@ -5,9 +5,14 @@ import com.BFF_paye_ton_kawa.Utils.JsonSingleResponse;
 import com.BFF_paye_ton_kawa.order.DTO.OrderRequestDTO;
 import com.BFF_paye_ton_kawa.order.DTO.OrderResponseDTO;
 import io.swagger.v3.core.util.Json;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 @RequestMapping("/orders")
@@ -24,12 +29,14 @@ public class OrderController {
     };
 
     @GetMapping("/{id}")
-    public JsonSingleResponse<OrderResponseDTO> getOrderById(@RequestParam String id) {
-        return orderService.getOrder(id);
+    public JsonSingleResponse<OrderResponseDTO> getOrderById(@RequestParam String id) throws Exception {
+
+            return orderService.getOrder(id);
+
     };
 
     @PostMapping("/")
-    public JsonSingleResponse<OrderResponseDTO> createOrder(@RequestBody OrderRequestDTO orderRequestDTO) {
+    public JsonSingleResponse<OrderResponseDTO> createOrder(@Valid @RequestBody OrderRequestDTO orderRequestDTO) {
         return orderService.orderCreation(orderRequestDTO);
     }
 
