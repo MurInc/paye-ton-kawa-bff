5188b1713614b214d32178fc5259e7a63ee1289f
Eliseo <eliseomurillo9@gmail.com>
2025-06-19 15:11:53 +0200

    chore: add fail test to test sonar

---
+++ src/test/java/com/BFF_paye_ton_kawa/TestFails.java
@@ -0,0 +1,13 @@
+package com.BFF_paye_ton_kawa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestFails {
+    @Test
+    void shouldFailAlways() {
+        fail("This test is supposed to fail.");
+    }
+}
+
