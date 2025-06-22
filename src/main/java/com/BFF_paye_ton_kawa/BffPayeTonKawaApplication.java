bdd609336f5976718342e991a9aa68557eb83605
Eliseo <eliseomurillo9@gmail.com>
2025-06-22 23:47:53 +0200

    fix: trace logs

--- src/main/java/com/BFF_paye_ton_kawa/BffPayeTonKawaApplication.java
+++ src/main/java/com/BFF_paye_ton_kawa/BffPayeTonKawaApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.actuate.autoconfigure.tracing.otlp.OtlpAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.metrics.export.otlp.OtlpMetricsExportAutoConfiguration.class
+})
 @RequestMapping("/api")
 public class BffPayeTonKawaApplication {
 
