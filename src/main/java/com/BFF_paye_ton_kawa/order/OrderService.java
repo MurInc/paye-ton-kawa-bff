37a4c38dcbd5514f761a3866e602ec73689fc164
Eliseo Murillo <95434225+eliseomurillo9@users.noreply.github.com>
2025-06-22 23:50:23 +0200

    Fix client schema (#8)
    * feat: add logs
    
    * chore: add logger
    
    * fix: trace logs
    
    * fix: remove app args

--- src/main/java/com/BFF_paye_ton_kawa/order/OrderService.java
+++ src/main/java/com/BFF_paye_ton_kawa/order/OrderService.java
@@ -5,18 +5,25 @@ import com.BFF_paye_ton_kawa.Utils.JsonSingleResponse;
 import com.BFF_paye_ton_kawa.config.ApiProperties;
 import com.BFF_paye_ton_kawa.order.DTO.OrderRequestDTO;
 import com.BFF_paye_ton_kawa.order.DTO.OrderResponseDTO;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RestController;
 @Service
 public class OrderService {
     private final RestTemplate restTemplate;
 
     ApiProperties apiPros;
+    private static final Logger log = LogManager.getLogger(Controller.class);
+
 
     public OrderService(RestTemplate restTemplate, ApiProperties apiProps) {
         this.restTemplate = restTemplate;
@@ -24,15 +31,17 @@ public class OrderService {
     }
     public JsonMultipleResponse<OrderResponseDTO> getAllOrders() {
         String url = apiPros.getOrderUrl();
-        return restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<JsonMultipleResponse<OrderResponseDTO>>() {
+        JsonMultipleResponse<OrderResponseDTO> response =  restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<JsonMultipleResponse<OrderResponseDTO>>() {
         }).getBody();
+        log.info("Fetching all orders from URL: {}", url);
+        return response;
     }
 
     public JsonSingleResponse<OrderResponseDTO> getOrder(String id) {
         String url = UriComponentsBuilder.fromUriString(apiPros.getOrderUrl())
                 .pathSegment(id)
                 .toUriString();
-
+        log.info("Fetching order with ID: {} from URL: {}", id, url);
         return restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<JsonSingleResponse<OrderResponseDTO>>() {
         }).getBody();
     }
@@ -40,12 +49,14 @@ public class OrderService {
     public JsonSingleResponse<OrderResponseDTO> orderCreation(OrderRequestDTO orderInformation) {
         String url = apiPros.getOrderUrl();
         HttpEntity<OrderRequestDTO> requestEntity = new HttpEntity<>(orderInformation);
+        log.info("Creating order at URL: {}", url);
         return restTemplate.exchange(url, HttpMethod.POST, requestEntity, new ParameterizedTypeReference<JsonSingleResponse<OrderResponseDTO>>() {
         }).getBody();
     }
 
     public void deleteOrder(String id) {
         String url = UriComponentsBuilder.fromUriString(apiPros.getOrderUrl()).pathSegment(id).toUriString();
+        log.info("Deleting order with ID: {} at URL: {}", id, url);
         restTemplate.delete(url);
     }
 }
