295767e8d69424dfeddac7691ff92740c62ff608
Eliseo <eliseomurillo9@gmail.com>
2025-06-22 23:49:29 +0200

    fix: remove app args

--- src/main/java/com/BFF_paye_ton_kawa/BffPayeTonKawaApplication.java
+++ src/main/java/com/BFF_paye_ton_kawa/BffPayeTonKawaApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.actuate.autoconfigure.tracing.otlp.OtlpAutoConfiguration.class,
-		org.springframework.boot.actuate.autoconfigure.metrics.export.otlp.OtlpMetricsExportAutoConfiguration.class
-})
+@SpringBootApplication
 @RequestMapping("/api")
 public class BffPayeTonKawaApplication {
 
