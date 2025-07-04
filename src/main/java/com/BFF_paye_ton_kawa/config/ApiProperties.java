474e38b9aaba38ed0dd4ae68349e8e401dcaa575
Eliseo <eliseomurillo9@gmail.com>
2025-07-04 16:22:30 +0200

    feat: add config verif

--- src/main/java/com/BFF_paye_ton_kawa/config/ApiProperties.java
+++ src/main/java/com/BFF_paye_ton_kawa/config/ApiProperties.java
@@ -1,16 +1,30 @@
     package com.BFF_paye_ton_kawa.config;
 
+    import jakarta.annotation.PostConstruct;
+    import jakarta.validation.constraints.NotBlank;
+    import org.hibernate.validator.constraints.URL;
     import org.springframework.boot.context.properties.ConfigurationProperties;
     import org.springframework.boot.context.properties.EnableConfigurationProperties;
     import org.springframework.context.annotation.Configuration;
     import org.springframework.stereotype.Component;
+    import org.springframework.validation.annotation.Validated;
 
     @Component
+    @Validated
     @ConfigurationProperties(prefix = "external.apis")
     public class ApiProperties {
+        @NotBlank(message = "Product API URL must not be blank")
+        @URL(message = "Product API URL must be a valid URL")
         private String productUrl;
+        @NotBlank(message = "Client API URL must not be blank")
+        @URL(message = "Product API URL must be a valid URL")
+
         private String clientUrl;
+        @NotBlank(message = "Order API URL must not be blank")
+        @URL(message = "Product API URL must be a valid URL")
+
         private String orderUrl;
+
 
         public String getProductUrl() {
             return productUrl;
@@ -35,4 +49,5 @@
         public void setOrderUrl(String orderUrl) {
             this.orderUrl = orderUrl;
         }
+
     }
